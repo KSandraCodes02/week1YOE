bc9c212f154255c3bc2c585b9e07a78bb985ff19
KSandraCodes02 <52667157+KSandraCodes02@users.noreply.github.com>
2019-07-09 13:04:31 -0400

    Add files via upload

---
+++ warmUp.java
@@ -0,0 +1,27 @@
+package day2;
+
+import java.util.Scanner;
+
+public class warmUp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+Scanner scnr = new Scanner(System.in);
+		
+		System.out.println("What is your name");
+		scnr.next();
+		System.out.println("what is your age");
+		scnr.next();
+		System.out.println("what is your favorite color");
+		scnr.next();
+		System.out.println("wwhat is your favorite number");
+		scnr.next();
+		System.out.println("thank you for participating");
+		
+		
+		scnr.close();
+	}
+
+}
